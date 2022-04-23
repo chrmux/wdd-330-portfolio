4dbf2dcc457750c224c2927272daffebd47c330f
chrmux <jnmarollano@gmail.com>
2022-04-24 01:00:21 +0800

    Added

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let theme = document.getElementById('theme');
+
+theme.onclick = () => {
+  theme.classList.toggle('fa-sun');
+
+  if (theme.classList.contains('fa-sun')) {
+    document.body.classList.add('active');
+  } else {
+    document.body.classList.remove('active');
+  }
+};
+
+var links = [
+  {
+    label: "Week1 notes",
+    url: "/notes/index.html"
+  },
+
+];
+
+for (var i in links) {
+  var li = document.createElement('li')
+  var a = document.createElement('a'),
+  li = document.createTextNode(links[i].label);
+  a.href = links[i].url;
+  a.appendChild(li);
+  document.querySelector('.content').appendChild(a);
+};
