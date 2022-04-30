891d9e2501e12783f3043bc498d13f966f8e330f
chrmux <jnmarollano@gmail.com>
2022-05-01 01:35:03 +0800

    Added

--- js/main.js
+++ js/main.js
@@ -18,22 +18,11 @@ var links = [
 ];
 
 for (var i in links) {
-  var li = document.createElement('li')
+  var text = document.createElement('text')
   var a = document.createElement('a'),
-  li = document.createTextNode(links[i].label);
+  text = document.createTextNode(links[i].label);
   a.href = links[i].url;
   a.appendChild(li);
   document.querySelector('.content').appendChild(a);
 };
 
-let app = document.querySelector('#app');
-
-let langs = ['TypeScript','HTML','CSS'];
-
-let nodes = langs.map(lang => {
-    let li = document.createElement('li');
-    li.textContent = lang;
-    return li;
-});
-
-app.append(...nodes);
