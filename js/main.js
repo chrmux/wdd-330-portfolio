6cfcd9f2d206c50e64f4f05b496c00e581f4c1f7
chrmux <jnmarollano@gmail.com>
2022-05-07 06:57:17 +0800

    Added

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-let theme = document.getElementById('theme');
-
-theme.onclick = () => {
-  theme.classList.toggle('fa-sun');
-
-  if (theme.classList.contains('fa-sun')) {
-    document.body.classList.add('active');
-  } else {
-    document.body.classList.remove('active');
-  }
-};
-
 var links = [
   {
     label: "Week1",
