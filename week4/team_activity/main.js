30224364e465c44038ec3e96d23655bc8d66df12
chrmux <jnmarollano@gmail.com>
2022-05-11 07:02:13 +0800

    Added

--- week4/team_activity/main.js
+++ week4/team_activity/main.js
@@ -1,19 +1,36 @@
-var currentPlayer = 1;
-            function clearBoard() {
-                let cells = document.getElementsByClassName('cell');
-                console.log(cells.length);
-                    for (var i = 0; i < cells.length; i++) {
-                        // console.log("Touch!");
-                        cells[i].innerText = "";
-                    } 
-            }
-            function markCell(id) {
-                console.log(id)
-                if(currentPlayer === 1) {
-                    document.getElementById(id).innerText = "X";
-                    currentPlayer = 2;
-                } else if (currentPlayer === 2) {
-                    document.getElementById(id).innerText = "O";
-                    currentPlayer = 1;
-                }
-            }
+// Core Requirements
+const reset = document.getElementById('reset');
+const cells = document.querySelectorAll('.cell');
+const divBoard = document.forms.ticTac;
+const player1 = {name: "player1", sign: "X"};
+const player2 = {name: "player2", sign: "0"};
+let player = player1;
+
+document.getElementById("currentPlayer").innerHTML = player.name;
+
+const getId = (e) => {
+  let cellName = e.target;
+  document.getElementById("currentPlayer").innerHTML = player.name;
+  cellName.innerHTML = player.sign;
+  if (player == player1) player = player2;
+  else player = player1;
+}
+  // console.log(e.target);
+
+divBoard.addEventListener('click', getId);
+
+
+// 03
+// Add a reset button.
+// When touched the reset button should remove all of the marks from the board.
+
+
+const resetBoard = (e) =>  {
+  e.preventDefault();
+  document.getElementById("currentPlayer").innerHTML = player1.name;
+  cells.forEach((c) => {
+    c.innerHTML = '';
+  });
+}
+
+reset.addEventListener('click', resetBoard, false);
