c633082099ec75bf5472acd4f277fdcdddcd1be1
chrmux <jnmarollano@gmail.com>
2022-05-17 06:01:10 +0800

    Added

---
+++ week5/team_activity/js/hikeController.js
@@ -0,0 +1,23 @@
+import HikeModel from './HikeModel.js';
+import HikesView from './HikesView.js';
+
+export default class HikesController {
+    constructor(parentId){
+        this.parentElement = document.getElementById(parentId);
+        this.hikeModel = new HikeModel();
+        console.log("constructor" + this.hikeModel.getAllHikes());
+        this.hikesView = new HikesView(parentId);
+    }
+    showHikeList() {
+        this.hikesView.renderHikeList(this.hikeModel.getAllHikes(), 'hikes');
+    }
+    showOneHike(hikeName){
+        let desiredHike = this.hikeModel.getHikeByName(hikeName);
+        console.log("controller: " + desiredHike);
+        this.hikesView.renderOneHikeLight(desiredHike);
+    }
+    addHikeListener(){
+        document.getElementById('hikes');
+        
+    }
+}
