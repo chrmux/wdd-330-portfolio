bbd3dafd1f8dcf53990ee5fbaec0639f6cfa8431
chrmux <jnmarollano@gmail.com>
2022-05-20 10:19:54 +0800

    Added

--- week5/team_activity/js/hikes.js
+++ week5/team_activity/js/hikes.js
@@ -39,37 +39,27 @@ const hikeList = [
   export default class Hikes {
     constructor(elementId) {
       this.parentElement = document.getElementById(elementId);
-      // we need a back button to return back to the list. This will build it and hide it. When we need it we just need to remove the 'hidden' class
       this.backButton = this.buildBackButton();
     }
-    // hikeList is not exported, and so it cannot be seen outside of this module.
     getAllHikes() {
       return hikeList;
     }
-    // For the first stretch we will need to get just one hike.
     getHikeByName(hikeName) {
       return this.getAllHikes().find(hike => hike.name === hikeName);
     }
-    //show a list of hikes in the parentElement
     showHikeList() {
       this.parentElement.innerHTML = '';
-      // notice that we use our getter above to grab the list instead of getting it directly...this makes it easier on us if our data source changes...
       renderHikeList(this.parentElement, this.getAllHikes());
       this.addHikeListener();
-      // make sure the back button is hidden
       this.backButton.classList.add('hidden');
     }
-    // show one hike with full details in the parentElement
     showOneHike(hikeName) {
       const hike = this.getHikeByName(hikeName);
       this.parentElement.innerHTML = '';
       this.parentElement.appendChild(renderOneHikeFull(hike));
-      // show the back button
       this.backButton.classList.remove('hidden');
     }
-    // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
     addHikeListener() {
-      // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
       const childrenArray = Array.from(this.parentElement.children);
       childrenArray.forEach(child => {
         child.addEventListener('touchend', e => {
@@ -88,8 +78,6 @@ const hikeList = [
       return backButton;
     }
   }
-  // End of Hikes class
-  // methods responsible for building HTML.  Why aren't these in the class?  They don't really need to be, and by moving them outside of the exported class, they cannot be called outside the module...they become private.
   function renderHikeList(parent, hikes) {
     hikes.forEach(hike => {
       parent.appendChild(renderOneHikeLight(hike));
