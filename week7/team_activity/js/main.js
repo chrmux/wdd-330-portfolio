a1112b4f340bf5ae131efba26c76deac3ef20995
chrmux <jnmarollano@gmail.com>
2022-06-03 09:41:53 +0800

    Added

--- week7/team_activity/js/main.js
+++ week7/team_activity/js/main.js
@@ -1,6 +1,5 @@
 import Hikes from './hikes.js';
 import Comment from './comments.js'
-//on load grab the array and insert it into the page
 const myHikes = new Hikes('hikes');
 const comments = new Comment('comments');
 console.log(comments);
