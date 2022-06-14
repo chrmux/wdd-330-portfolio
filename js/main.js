200c81a8074d0ecef7af58e87acfa68fd5ea4777
chrmux <jnmarollano@gmail.com>
2022-06-15 07:49:54 +0800

    Added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-var links = [
-  {
-    label: "Week1",
-    url: "https://chrmux.github.io/wdd-330-portfolio/week1/notes/index.html"
-  },
-];
-
-for (var i = 0; i < links.length; i++){
-  var li = document.createElement('li')
-  var a = document.createElement('a'),
-  li = document.createTextNode(links[i].label);
-  a.href = links[i].url;
-  a.appendChild(li);
-  document.querySelector('.content').appendChild(a);
-};
-
 (function() {
   'use strict';
 
