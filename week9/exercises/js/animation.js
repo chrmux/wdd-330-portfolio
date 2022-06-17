6a094441c58b4f85fb63a15545ed2da39a9fce41
chrmux <jnmarollano@gmail.com>
2022-06-18 02:07:53 +0800

    Added

--- week9/exercises/js/animation.js
+++ week9/exercises/js/animation.js
@@ -1,29 +1,10 @@
-
-// square
 const squareElement = document.getElementById('square');
 let angle = 0;
 
-setInterval( () => {
-    angle = (angle + 2) % 360;
+function rotate() {
+    angle = (angle + 2)%360;
     squareElement.style.transform = `rotate(${angle}deg)`
-}, 1000/60);
+    window.requestAnimationFrame(rotate);
+}
 
-
-// container
-
-function myMove() {
-    let id = null;
-    const elem = document.getElementById("animate");
-    let pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame() {
-      if (pos == 350) {
-        clearInterval(id);
-      } else {
-        pos++;
-        elem.style.top = pos + 'px';
-        elem.style.left = pos + 'px';
-      }
-    }
-  }
+const id = requestAnimationFrame(rotate);
